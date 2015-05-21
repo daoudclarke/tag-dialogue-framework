f7f96abe18661c3b6e53baee9c4d3680677fe3d8
julieweeds <julie.weeds@gmail.com>
2015-05-21 10:09:19 +0100

    testing

--- src/main/java/uk/ac/susx/tag/dialoguer/dialogue/handling/handlers/taxiServiceHandlers/orderTaxiMethod.java
+++ src/main/java/uk/ac/susx/tag/dialoguer/dialogue/handling/handlers/taxiServiceHandlers/orderTaxiMethod.java
@@ -1,5 +1,6 @@
 package uk.ac.susx.tag.dialoguer.dialogue.handling.handlers.taxiServiceHandlers;
 
+import com.google.common.collect.Lists;
 import uk.ac.susx.tag.dialoguer.dialogue.components.Dialogue;
 import uk.ac.susx.tag.dialoguer.dialogue.components.Intent;
 import uk.ac.susx.tag.dialoguer.dialogue.components.Response;
@@ -12,10 +13,11 @@ import java.util.stream.Collectors;
 /**
  * Created by juliewe on 19/05/2015.
  */
-public class orderTaxiMethod implements Handler.IntentHandler{
+public class OrderTaxiMethod implements Handler.IntentHandler{
     @Override
     public Response handle(Intent intent, Dialogue dialogue, Object resource) {
         System.err.println("orderTaxi intent handler fired");
+        dialogue.pushFocus(TaxiServiceHandler.confirmCompletionResponse);
         dialogue.pushFocus(TaxiServiceHandler.confirmResponse);
         handleCapacity(intent,dialogue);
         handleTime(intent,dialogue);
@@ -25,64 +27,35 @@ public class orderTaxiMethod implements Handler.IntentHandler{
         return TaxiServiceHandler.processStack(dialogue);
     }
 
-    private void handleCapacity(Intent i, Dialogue d){
+    static void handleCapacity(Intent i, Dialogue d){
         //check for multiple and empty slots
-        List<String> values = i.getSlotValuesByType(TaxiServiceHandler.capacitySlot);
-        if (values.isEmpty()){
-            //insert default
-            i.fillSlot(TaxiServiceHandler.capacitySlot,"4");
-        } else {
-            //check values are valid - currently assume all ok
-            values.stream().forEach(value -> System.err.println("Capacity: " + value));
-        }
-        if(values.size()>1){
-            d.pushFocus(TaxiServiceHandler.chooseCapacityResponse);
-        }
-
+        List<String> values = FollowupProblemHandler.validate(i,TaxiServiceHandler.capacitySlot);
+        generateResponse(values,TaxiServiceHandler.capacitySlot, Lists.newArrayList(TaxiServiceHandler.chooseCapacityResponse),d);
     }
-    private void handleTime(Intent i, Dialogue d){
-        List<String> values = i.getSlotValuesByType(TaxiServiceHandler.timeSlot);
+
+    static void handleTime(Intent i, Dialogue d){
+        List<String> values = FollowupProblemHandler.validate(i,TaxiServiceHandler.timeSlot);
+        generateResponse(values,TaxiServiceHandler.timeSlot,Lists.newArrayList(TaxiServiceHandler.chooseTimeResponse),d);
+    }
+
+    static void handleDestination(Intent i, Dialogue d){
+        List<String> values = FollowupProblemHandler.validate(i,TaxiServiceHandler.destinationSlot);
+        generateResponse(values,TaxiServiceHandler.destinationSlot,Lists.newArrayList(TaxiServiceHandler.chooseDestinationResponse,TaxiServiceHandler.respecifyDestinationResponse),d);
+    }
+    static void handlePickup(Intent i, Dialogue d){
+        List<String> values = FollowupProblemHandler.validate(i,TaxiServiceHandler.pickupSlot);
+        generateResponse(values,TaxiServiceHandler.pickupSlot,Lists.newArrayList(TaxiServiceHandler.choosePickupResponse,TaxiServiceHandler.respecifyDestinationResponse),d);
+    }
+
+    static void generateResponse(List<String> values,String slotname, List<String> responsenames, Dialogue d){
         if(values.isEmpty()){
-            //insert default
-            i.fillSlot(TaxiServiceHandler.timeSlot,"ASAP");
-        } else {
-            //check values are valid - currently assume all ok
-            values.stream().forEach(value->System.err.println("Time: "+value));
+            d.pushFocus(responsenames.get(1));
         }
         if(values.size()>1){
-            d.pushFocus(TaxiServiceHandler.chooseTimeResponse);
+            d.pushFocus(responsenames.get(0));
+            d.putToWorkingMemory("slot_to_choose",slotname);
         }
 
     }
-    private void handleDestination(Intent i, Dialogue d){
-        List<String> values = i.getSlotValuesByType(TaxiServiceHandler.destinationSlot);
-        //check values are valid-currently assume all ok but will do filter
-        values.stream().forEach(value->System.err.println("Destination: "+value));
-        values.stream().filter(value->validLocation(value)).collect(Collectors.toList());
 
-        if(values.isEmpty()){
-            d.pushFocus(TaxiServiceHandler.respecifyDestinationResponse);
-        }
-        if(values.size()>1){
-            d.pushFocus(TaxiServiceHandler.chooseDestinationResponse);
-        }
-
-    }
-    private void handlePickup(Intent i, Dialogue d){
-        List<String> values = i.getSlotValuesByType(TaxiServiceHandler.pickupSlot);
-        //check values are valid-currently assume all ok but will do filter
-        values.stream().forEach(value->System.err.println("Pickup: "+value));
-        values.stream().filter(value->validLocation(value)).collect(Collectors.toList());
-
-        if(values.isEmpty()){
-            d.pushFocus(TaxiServiceHandler.respecifyPickupResponse);
-        }
-        if(values.size()>1){
-            d.pushFocus(TaxiServiceHandler.choosePickupResponse);
-        }
-    }
-
-    private boolean validLocation(String value){
-        return true;
-    }
 }
